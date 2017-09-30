33493c160943d8d68ee1a795e62b16747fc4b741
mumblingkraken <32409062+mumblingkraken@users.noreply.github.com>
2017-09-29 23:32:58 -0700

    Add project files.

---
+++ PROG120_Kirgan_Joe_HW2/VendingMachineUI.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using VendingMachine;
+
+namespace PROG120_Kirgan_Joe_HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Machine machine1 = new Machine();
+
+            string uiCommand;
+
+            Console.WriteLine();
+            Console.WriteLine("Welcome to a virtual coke machine please choose from the following;");
+            Console.WriteLine();
+            Console.WriteLine("P to enter a dollar.");
+            Console.WriteLine("B to buy a coke.");
+            Console.WriteLine("R to get a refund of remaining funds");
+            Console.WriteLine("Q to quit the virtual coke machine");
+            Console.Write("Your choice:");
+            uiCommand = Console.ReadLine();
+            uiCommand = uiCommand.ToUpper();
+            
+            while (uiCommand !="Q")
+            {
+                switch (uiCommand)
+                {
+                    default:
+                       Console.WriteLine();
+                       Console.WriteLine("Invalid please try again");
+                       break;
+                    case "P":
+                        Console.WriteLine();
+                        machine1.AcceptCash();
+                        Console.WriteLine("Thank you your new balance is ${0}", machine1.PubBalance);
+                        break;
+                    case "B":
+                        
+                        machine1.BuyCoke();
+                        int rtrnValue = machine1.BuyCoke();
+                        Console.WriteLine("{0}",rtrnValue);
+                        if (rtrnValue == 1)// please see notes on class
+                        {
+                            Console.WriteLine("Enjoy your coke! Your remaining balance is {0}",machine1.PubBalance);
+                        }
+                        
+                        else if (rtrnValue == 2)
+                        {
+                            Console.WriteLine("Please add money to buy a coke");
+                        }
+                        else if (rtrnValue ==3)// as below 
+                        {
+                            Console.WriteLine("There arent any cokes left and you have no money in the machine please come back after we've restocked");
+                        }
+                        else if (rtrnValue ==4)// will tell us there are no more cokes left if we just bought the last one
+                        {
+                            Console.WriteLine("There are no more cokes hit R for a refund of any remaining funds on the next screen then come back after we've restocked");
+                        }
+                        
+                        Console.WriteLine();
+                        break;
+                        case "R":
+                        Console.WriteLine();
+                        Console.WriteLine("You will be refunded ${0}",machine1.PubBalance);
+                        machine1.GiveRefund();
+                        break;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Welcome to a virtual coke machine please choose from the following;");
+                Console.WriteLine();
+                Console.WriteLine("P to enter a dollar.");
+                Console.WriteLine("B to buy a coke.");
+                Console.WriteLine("R to get a refund of remaining funds");
+                Console.WriteLine("Q to quit the virtual coke machine");
+                Console.Write("Your choice:");
+                uiCommand = Console.ReadLine();
+                uiCommand = uiCommand.ToUpper();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Thank you for using the virtual coke machine");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VendingMachine/Machine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    public class Machine
+    {
+        private int _Balance { get; set; } = 0;
+        private int _MachineCap { get; set; } = 5;
+
+        public int PubMachineCap
+        {
+            get { return _MachineCap; }
+            
+        }
+
+        public int PubBalance
+        {
+            get { return _Balance; }
+           
+        }
+        public int BuyCoke()
+        {
+            int header = 777;
+            
+            if (_Balance > 0 && _MachineCap > 0)
+            {
+                _Balance -= 1; 
+                _MachineCap -= 1;
+                return 1;
+                
+            }
+            else if (_Balance == 0)
+            {
+                return 2;
+            }
+            else if (_MachineCap == 0)
+            {
+
+            }
+            else if (_Balance == 0 && _MachineCap == 0)
+            {
+                
+            }
+
+            return header;
+
+        }
+        
+        public void AcceptCash()
+        {
+            { _Balance +=1; }
+        }
+
+        public int GiveRefund()
+        {
+
+            if (_Balance > 0)
+            {
+                { _Balance -= _Balance; }
+            }
+            return 0;
+        }
+    }
+}
